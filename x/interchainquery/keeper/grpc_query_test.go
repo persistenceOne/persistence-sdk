a2503731c0610b43f5010f96d58d00d3adf8590d
arhamchordia <chordia.1@iitj.ac.in>
2022-08-24 00:08:52 +0400

    Added interchainquery tests.

---
+++ x/interchainquery/keeper/grpc_query_test.go
@@ -0,0 +1,40 @@
+package keeper_test
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+
+	icqtypes "github.com/persistenceOne/persistence-sdk/x/interchainquery/types"
+)
+
+func (s *KeeperTestSuite) TestQueries() {
+	bondedQuery := stakingtypes.QueryValidatorsRequest{Status: stakingtypes.BondStatusBonded}
+	bz1, err := bondedQuery.Marshal()
+	s.NoError(err)
+
+	query := s.GetSimApp(s.chainA).InterchainQueryKeeper.NewQuery(
+		s.chainA.GetContext(),
+		"",
+		s.path.EndpointB.ConnectionID,
+		s.chainB.ChainID,
+		"cosmos.staking.v1beta1.Query/Validators",
+		bz1,
+		sdk.NewInt(200),
+		"",
+		0,
+	)
+
+	// set the query
+	s.GetSimApp(s.chainA).InterchainQueryKeeper.SetQuery(s.chainA.GetContext(), *query)
+
+	icqsrvSrv := icqtypes.QuerySrvrServer(s.GetSimApp(s.chainA).InterchainQueryKeeper)
+
+	res, err := icqsrvSrv.Queries(sdk.WrapSDKContext(s.chainA.GetContext()), &icqtypes.QueryRequestsRequest{ConnectionId: s.path.EndpointB.ConnectionID})
+	s.NoError(err)
+	s.Len(res.Queries, 1)
+	s.Equal(s.path.EndpointB.ConnectionID, res.Queries[0].ConnectionId)
+	s.Equal(s.chainB.ChainID, res.Queries[0].ChainId)
+	s.Equal("cosmos.staking.v1beta1.Query/Validators", res.Queries[0].QueryType)
+	s.Equal(sdk.NewInt(200), res.Queries[0].Period)
+	s.Equal("", res.Queries[0].CallbackId)
+}
