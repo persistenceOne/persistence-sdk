05d350b718c14d311e816d0c4028e8a0614a855d
puneetmahajan <puneet@persistence.one>
2022-08-24 15:12:58 +0400

    add error handling.

--- x/ibchooker/types/hooks.go
+++ x/ibchooker/types/hooks.go
@@ -4,6 +4,7 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/ibc-go/v3/modules/core/04-channel/types"
 	"github.com/cosmos/ibc-go/v3/modules/core/exported"
+	"github.com/persistenceOne/persistence-sdk/utils"
 )
 
 var _ IBCHandshakeHooks = MultiIBCHandshakeHooks{}
@@ -17,18 +18,75 @@ func NewMultiStakingHooks(hooks ...IBCHandshakeHooks) MultiIBCHandshakeHooks {
 
 func (h MultiIBCHandshakeHooks) OnRecvPacket(ctx sdk.Context, packet types.Packet, relayer sdk.AccAddress, transferAck exported.Acknowledgement) {
 	for i := range h {
-		h[i].OnRecvPacket(ctx, packet, relayer, transferAck)
+		panicCatchingOnRecvPacketFnHook(ctx, h[i].OnRecvPacket, packet, relayer, transferAck)
 	}
 }
 
 func (h MultiIBCHandshakeHooks) OnAcknowledgementPacket(ctx sdk.Context, packet types.Packet, acknowledgement []byte, relayer sdk.AccAddress, transferAckErr error) {
 	for i := range h {
-		h[i].OnAcknowledgementPacket(ctx, packet, acknowledgement, relayer, transferAckErr)
+		panicCatchingOnAcknowledgementPacketFnHook(ctx, h[i].OnAcknowledgementPacket, packet, acknowledgement, relayer, transferAckErr)
 	}
 }
 
 func (h MultiIBCHandshakeHooks) OnTimeoutPacket(ctx sdk.Context, packet types.Packet, relayer sdk.AccAddress, transferTimeoutErr error) {
 	for i := range h {
-		h[i].OnTimeoutPacket(ctx, packet, relayer, transferTimeoutErr)
+		panicCatchingOnTimeoutPacketFnHook(ctx, h[i].OnTimeoutPacket, packet, relayer, transferTimeoutErr)
 	}
+}
+
+// Panic catching Fn implementations
+// We want to be using cacheContext in case of failure not to write entire code block
+func panicCatchingOnRecvPacketFnHook(
+	ctx sdk.Context,
+	onRecvPacketFn func(ctx sdk.Context, packet types.Packet, relayer sdk.AccAddress, transferAck exported.Acknowledgement),
+	packet types.Packet,
+	relayer sdk.AccAddress,
+	transferAck exported.Acknowledgement,
+) {
+	defer func() {
+		if recovErr := recover(); recovErr != nil {
+			utils.PrintPanicRecoveryError(ctx, recovErr)
+		}
+	}()
+
+	cacheCtx, write := ctx.CacheContext()
+	onRecvPacketFn(cacheCtx, packet, relayer, transferAck)
+	write()
+}
+
+func panicCatchingOnAcknowledgementPacketFnHook(
+	ctx sdk.Context,
+	onAcknowledgementPacketFn func(ctx sdk.Context, packet types.Packet, acknowledgement []byte, relayer sdk.AccAddress, transferAckErr error),
+	packet types.Packet,
+	acknowledgement []byte,
+	relayer sdk.AccAddress,
+	transferAckErr error,
+) {
+	defer func() {
+		if recovErr := recover(); recovErr != nil {
+			utils.PrintPanicRecoveryError(ctx, recovErr)
+		}
+	}()
+
+	cacheCtx, write := ctx.CacheContext()
+	onAcknowledgementPacketFn(cacheCtx, packet, acknowledgement, relayer, transferAckErr)
+	write()
+}
+
+func panicCatchingOnTimeoutPacketFnHook(
+	ctx sdk.Context,
+	onTimeoutPacketFn func(ctx sdk.Context, packet types.Packet, relayer sdk.AccAddress, transferTimeoutErr error),
+	packet types.Packet,
+	relayer sdk.AccAddress,
+	transferTimeoutErr error,
+) {
+	defer func() {
+		if recovErr := recover(); recovErr != nil {
+			utils.PrintPanicRecoveryError(ctx, recovErr)
+		}
+	}()
+
+	cacheCtx, write := ctx.CacheContext()
+	onTimeoutPacketFn(cacheCtx, packet, relayer, transferTimeoutErr)
+	write()
 }
