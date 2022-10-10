6668de97a2c253a3a59d2b25825240e2be6db5c1
Puneet <59960662+puneet2019@users.noreply.github.com>
2022-10-10 17:54:50 +0400

    update deps and proto compiled files (#371)
    * update deps and proto compiled files
    
    * fix lint

--- x/epochs/keeper/abci_test.go
+++ x/epochs/keeper/abci_test.go
@@ -17,7 +17,7 @@ import (
 
 // This test is responsible for testing how epochs increment based off
 // of their initial conditions, and subsequent block height / times.
-func (suite KeeperTestSuite) TestEpochInfoBeginBlockChanges() {
+func (suite *KeeperTestSuite) TestEpochInfoBeginBlockChanges() {
 	block1Time := time.Unix(1656907200, 0).UTC()
 
 	const defaultIdentifier = "hourly"
