cbb926eeb46ee3422dad8acb45ba630d5208227b
puneetmahajan <puneet@persistence.one>
2023-03-16 23:14:42 +0400

    fix lint

--- x/ibchooker/types/hooks.go
+++ x/ibchooker/types/hooks.go
@@ -23,6 +23,7 @@ func (h MultiIBCHandshakeHooks) OnRecvPacket(ctx sdk.Context, packet types.Packe
 			//nolint:scopelint
 			return h[i].OnRecvPacket(ctx, packet, relayer, transferAck)
 		}
+
 		err := utils.ApplyFuncIfNoError(ctx, wrappedHookFn)
 		if err != nil {
 			ctx.Logger().Error("Error occurred in calling OnRecvPacket hooks, ", "err: ", err, "module:", ModuleName, "index:", i)
@@ -38,6 +39,7 @@ func (h MultiIBCHandshakeHooks) OnAcknowledgementPacket(ctx sdk.Context, packet 
 			//nolint:scopelint
 			return h[i].OnAcknowledgementPacket(ctx, packet, acknowledgement, relayer, transferAckErr)
 		}
+
 		err := utils.ApplyFuncIfNoError(ctx, wrappedHookFn)
 		if err != nil {
 			ctx.Logger().Error("Error occurred in calling OnAcknowledgementPacket hooks, ", "err: ", err, "module:", ModuleName, "index:", i)
@@ -53,6 +55,7 @@ func (h MultiIBCHandshakeHooks) OnTimeoutPacket(ctx sdk.Context, packet types.Pa
 			//nolint:scopelint
 			return h[i].OnTimeoutPacket(ctx, packet, relayer, transferTimeoutErr)
 		}
+
 		err := utils.ApplyFuncIfNoError(ctx, wrappedHookFn)
 		if err != nil {
 			ctx.Logger().Error("Error occurred in calling OnTimeoutPacket hooks, ", "err: ", err, "module:", ModuleName, "index:", i)
