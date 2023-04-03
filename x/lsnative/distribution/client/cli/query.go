persistenceOne/pstake-native

lsnative: incremental diff containing distilled patch for adding LSM to 0.46.11
This diff contains:
* slashing module changes
* staking module changes
* genutil module changes
* distribution module changes
* staking proto changes
* distribution proto changes
* added new protocgen.sh

x/lsnative/distribution/client/cli/query.go
@@ -11,7 +11,8 @@ import (
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/version"
-	"github.com/cosmos/cosmos-sdk/x/distribution/types"
+	sdkdistr "github.com/cosmos/cosmos-sdk/x/distribution/types"
+	"github.com/persistenceOne/pstake-native/v2/x/lsnative/distribution/types"
 )
 
 // GetQueryCmd returns the cli query commands for this module
@@ -31,6 +32,7 @@ func GetQueryCmd() *cobra.Command {
 		GetCmdQueryValidatorSlashes(),
 		GetCmdQueryDelegatorRewards(),
 		GetCmdQueryCommunityPool(),
+		GetCmdQueryTokenizeShareRecordReward(),
 	)
 
 	return distQueryCmd
@@ -260,7 +262,7 @@ $ %s query distribution rewards %s1gghjut3ccd8ay0zduzj64hwre2fxs9ld75ru9p %s1ggh
 
 				res, err := queryClient.DelegationRewards(
 					ctx,
-					&types.QueryDelegationRewardsRequest{DelegatorAddress: delegatorAddr.String(), ValidatorAddress: validatorAddr.String()},
+					&sdkdistr.QueryDelegationRewardsRequest{DelegatorAddress: delegatorAddr.String(), ValidatorAddress: validatorAddr.String()},
 				)
 				if err != nil {
 					return err
@@ -308,6 +310,51 @@ $ %s query distribution community-pool
 			queryClient := types.NewQueryClient(clientCtx)
 
 			res, err := queryClient.CommunityPool(cmd.Context(), &types.QueryCommunityPoolRequest{})
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+	return cmd
+}
+
+// GetCmdQueryTokenizeShareRecordReward implements the query tokenize share record rewards
+func GetCmdQueryTokenizeShareRecordReward() *cobra.Command {
+	bech32PrefixAccAddr := sdk.GetConfig().GetBech32AccountAddrPrefix()
+
+	cmd := &cobra.Command{
+		Use:   "tokenize-share-record-rewards [owner]",
+		Args:  cobra.ExactArgs(1),
+		Short: "Query distribution tokenize share record rewards",
+		Long: strings.TrimSpace(
+			fmt.Sprintf(`Query the query tokenize share record rewards.
+
+Example:
+$ %s query distribution tokenize-share-record-rewards %s1gghjut3ccd8ay0zduzj64hwre2fxs9ldmqhffj
+`,
+				version.AppName, bech32PrefixAccAddr,
+			),
+		),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+			queryClient := types.NewQueryClient(clientCtx)
+
+			ownerAddr, err := sdk.AccAddressFromBech32(args[0])
+			if err != nil {
+				return err
+			}
+
+			res, err := queryClient.TokenizeShareRecordReward(
+				cmd.Context(),
+				&types.QueryTokenizeShareRecordRewardRequest{OwnerAddress: ownerAddr.String()},
+			)
 			if err != nil {
 				return err
 			}

x/lsnative/distribution/simulation/proposals.go
@@ -6,8 +6,8 @@ import (
 	simappparams "github.com/cosmos/cosmos-sdk/simapp/params"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	simtypes "github.com/cosmos/cosmos-sdk/types/simulation"
-	"github.com/cosmos/cosmos-sdk/x/distribution/keeper"
-	"github.com/cosmos/cosmos-sdk/x/distribution/types"
+	"github.com/persistenceOne/pstake-native/v2/x/lsnative/distribution/keeper"
+	"github.com/persistenceOne/pstake-native/v2/x/lsnative/distribution/types"
 	"github.com/cosmos/cosmos-sdk/x/simulation"
 )
 

x/lsnative/slashing/keeper/keeper.go
@@ -3,13 +3,13 @@ package keeper
 import (
 	"fmt"
 
-	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	"github.com/tendermint/tendermint/libs/log"
 
 	"github.com/cosmos/cosmos-sdk/codec"
 	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
+	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/cosmos/cosmos-sdk/x/slashing/types"
+	"github.com/persistenceOne/pstake-native/v2/x/lsnative/slashing/types"
 )
 
 // Keeper of the slashing store

x/lsnative/slashing/simulation/operations.go
@@ -11,9 +11,9 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	simtypes "github.com/cosmos/cosmos-sdk/types/simulation"
 	"github.com/cosmos/cosmos-sdk/x/simulation"
-	"github.com/cosmos/cosmos-sdk/x/slashing/keeper"
-	"github.com/cosmos/cosmos-sdk/x/slashing/types"
-	stakingkeeper "github.com/cosmos/cosmos-sdk/x/staking/keeper"
+	"github.com/persistenceOne/pstake-native/v2/x/lsnative/slashing/keeper"
+	"github.com/persistenceOne/pstake-native/v2/x/lsnative/slashing/types"
+	stakingkeeper "github.com/persistenceOne/pstake-native/v2/x/lsnative/staking/keeper"
 )
 
 // Simulation operation weights constants
@@ -109,17 +109,13 @@ func SimulateMsgUnjail(ak types.AccountKeeper, bk types.BankKeeper, k keeper.Kee
 		// - validator is still in jailed period
 		// - self delegation too low
 		if info.Tombstoned ||
-			ctx.BlockHeader().Time.Before(info.JailedUntil) ||
-			validator.TokensFromShares(selfDel.GetShares()).TruncateInt().LT(validator.GetMinSelfDelegation()) {
+			ctx.BlockHeader().Time.Before(info.JailedUntil) {
 			if res != nil && err == nil {
 				if info.Tombstoned {
 					return simtypes.NewOperationMsg(msg, true, "", nil), nil, errors.New("validator should not have been unjailed if validator tombstoned")
 				}
 				if ctx.BlockHeader().Time.Before(info.JailedUntil) {
 					return simtypes.NewOperationMsg(msg, true, "", nil), nil, errors.New("validator unjailed while validator still in jail period")
-				}
-				if validator.TokensFromShares(selfDel.GetShares()).TruncateInt().LT(validator.GetMinSelfDelegation()) {
-					return simtypes.NewOperationMsg(msg, true, "", nil), nil, errors.New("validator unjailed even though self-delegation too low")
 				}
 			}
 			// msg failed as expected

x/lsnative/staking/client/cli/flags.go
@@ -3,7 +3,7 @@ package cli
 import (
 	flag "github.com/spf13/pflag"
 
-	"github.com/cosmos/cosmos-sdk/x/staking/types"
+	"github.com/persistenceOne/pstake-native/v2/x/lsnative/staking/types"
 )
 
 const (
@@ -25,8 +25,6 @@ const (
 	FlagCommissionRate          = "commission-rate"
 	FlagCommissionMaxRate       = "commission-max-rate"
 	FlagCommissionMaxChangeRate = "commission-max-change-rate"
-
-	FlagMinSelfDelegation = "min-self-delegation"
 
 	FlagGenesisFormat = "genesis-format"
 	FlagNodeID        = "node-id"
@@ -56,13 +54,6 @@ func FlagSetCommissionCreate() *flag.FlagSet {
 	fs.String(FlagCommissionMaxRate, "", "The maximum commission rate percentage")
 	fs.String(FlagCommissionMaxChangeRate, "", "The maximum commission change rate percentage (per day)")
 
-	return fs
-}
-
-// FlagSetMinSelfDelegation Returns the FlagSet used for minimum set delegation.
-func FlagSetMinSelfDelegation() *flag.FlagSet {
-	fs := flag.NewFlagSet("", flag.ContinueOnError)
-	fs.String(FlagMinSelfDelegation, "", "The minimum self delegation required on the validator")
 	return fs
 }
 

x/lsnative/staking/keeper/pool.go
@@ -4,7 +4,7 @@ import (
 	"cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
-	"github.com/cosmos/cosmos-sdk/x/staking/types"
+	"github.com/persistenceOne/pstake-native/v2/x/lsnative/staking/types"
 )
 
 // GetBondedPool returns the bonded tokens pool's module account

x/lsnative/staking/keeper/tokenize_share_record.go
@@ -1,0 +1,150 @@
+package keeper
+
+import (
+	"fmt"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	gogotypes "github.com/gogo/protobuf/types"
+
+	"github.com/persistenceOne/pstake-native/v2/x/lsnative/staking/types"
+)
+
+func (k Keeper) GetLastTokenizeShareRecordId(ctx sdk.Context) uint64 {
+	store := ctx.KVStore(k.storeKey)
+	bytes := store.Get(types.LastTokenizeShareRecordIdKey)
+	if bytes == nil {
+		return 0
+	}
+	return sdk.BigEndianToUint64(bytes)
+}
+
+func (k Keeper) SetLastTokenizeShareRecordId(ctx sdk.Context, id uint64) {
+	store := ctx.KVStore(k.storeKey)
+	store.Set(types.LastTokenizeShareRecordIdKey, sdk.Uint64ToBigEndian(id))
+}
+
+func (k Keeper) GetTokenizeShareRecord(ctx sdk.Context, id uint64) (tokenizeShareRecord types.TokenizeShareRecord, err error) {
+	store := ctx.KVStore(k.storeKey)
+
+	bz := store.Get(types.GetTokenizeShareRecordByIndexKey(id))
+	if bz == nil {
+		return tokenizeShareRecord, sdkerrors.Wrap(types.ErrTokenizeShareRecordNotExists, fmt.Sprintf("tokenizeShareRecord %d does not exist", id))
+	}
+
+	k.cdc.MustUnmarshal(bz, &tokenizeShareRecord)
+	return tokenizeShareRecord, nil
+}
+
+func (k Keeper) GetTokenizeShareRecordsByOwner(ctx sdk.Context, owner sdk.AccAddress) (tokenizeShareRecords []types.TokenizeShareRecord) {
+	store := ctx.KVStore(k.storeKey)
+
+	var it sdk.Iterator = sdk.KVStorePrefixIterator(store, types.GetTokenizeShareRecordIdsByOwnerPrefix(owner))
+	defer it.Close()
+
+	for ; it.Valid(); it.Next() {
+		var id gogotypes.UInt64Value
+		k.cdc.MustUnmarshal(it.Value(), &id)
+
+		tokenizeShareRecord, err := k.GetTokenizeShareRecord(ctx, id.Value)
+		if err != nil {
+			continue
+		}
+		tokenizeShareRecords = append(tokenizeShareRecords, tokenizeShareRecord)
+	}
+	return
+}
+
+func (k Keeper) GetTokenizeShareRecordByDenom(ctx sdk.Context, denom string) (types.TokenizeShareRecord, error) {
+	store := ctx.KVStore(k.storeKey)
+	bz := store.Get(types.GetTokenizeShareRecordIdByDenomKey(denom))
+	if bz == nil {
+		return types.TokenizeShareRecord{}, fmt.Errorf("tokenize share record not found from denom: %s", denom)
+	}
+
+	var id gogotypes.UInt64Value
+	k.cdc.MustUnmarshal(bz, &id)
+
+	return k.GetTokenizeShareRecord(ctx, id.Value)
+}
+
+func (k Keeper) GetAllTokenizeShareRecords(ctx sdk.Context) (tokenizeShareRecords []types.TokenizeShareRecord) {
+	store := ctx.KVStore(k.storeKey)
+
+	var it sdk.Iterator = sdk.KVStorePrefixIterator(store, types.TokenizeShareRecordPrefix)
+	defer it.Close()
+
+	for ; it.Valid(); it.Next() {
+		var tokenizeShareRecord types.TokenizeShareRecord
+		k.cdc.MustUnmarshal(it.Value(), &tokenizeShareRecord)
+
+		tokenizeShareRecords = append(tokenizeShareRecords, tokenizeShareRecord)
+	}
+	return
+}
+
+func (k Keeper) AddTokenizeShareRecord(ctx sdk.Context, tokenizeShareRecord types.TokenizeShareRecord) error {
+	if k.hasTokenizeShareRecord(ctx, tokenizeShareRecord.Id) {
+		return sdkerrors.Wrapf(types.ErrTokenizeShareRecordAlreadyExists, "TokenizeShareRecord already exists: %d", tokenizeShareRecord.Id)
+	}
+
+	k.setTokenizeShareRecord(ctx, tokenizeShareRecord)
+
+	owner, err := sdk.AccAddressFromBech32(tokenizeShareRecord.Owner)
+	if err != nil {
+		return err
+	}
+
+	k.setTokenizeShareRecordWithOwner(ctx, owner, tokenizeShareRecord.Id)
+	k.setTokenizeShareRecordWithDenom(ctx, tokenizeShareRecord.GetShareTokenDenom(), tokenizeShareRecord.Id)
+
+	return nil
+}
+
+func (k Keeper) DeleteTokenizeShareRecord(ctx sdk.Context, recordId uint64) error {
+	record, err := k.GetTokenizeShareRecord(ctx, recordId)
+	if err != nil {
+		return err
+	}
+	owner, err := sdk.AccAddressFromBech32(record.Owner)
+	if err != nil {
+		return err
+	}
+
+	store := ctx.KVStore(k.storeKey)
+	store.Delete(types.GetTokenizeShareRecordByIndexKey(recordId))
+	store.Delete(types.GetTokenizeShareRecordIdByOwnerAndIdKey(owner, recordId))
+	store.Delete(types.GetTokenizeShareRecordIdByDenomKey(record.GetShareTokenDenom()))
+	return nil
+}
+
+func (k Keeper) hasTokenizeShareRecord(ctx sdk.Context, id uint64) bool {
+	store := ctx.KVStore(k.storeKey)
+	return store.Has(types.GetTokenizeShareRecordByIndexKey(id))
+}
+
+func (k Keeper) setTokenizeShareRecord(ctx sdk.Context, tokenizeShareRecord types.TokenizeShareRecord) {
+	store := ctx.KVStore(k.storeKey)
+	bz := k.cdc.MustMarshal(&tokenizeShareRecord)
+
+	store.Set(types.GetTokenizeShareRecordByIndexKey(tokenizeShareRecord.Id), bz)
+}
+
+func (k Keeper) setTokenizeShareRecordWithOwner(ctx sdk.Context, owner sdk.AccAddress, id uint64) {
+	store := ctx.KVStore(k.storeKey)
+	bz := k.cdc.MustMarshal(&gogotypes.UInt64Value{Value: id})
+
+	store.Set(types.GetTokenizeShareRecordIdByOwnerAndIdKey(owner, id), bz)
+}
+
+func (k Keeper) deleteTokenizeShareRecordWithOwner(ctx sdk.Context, owner sdk.AccAddress, id uint64) {
+	store := ctx.KVStore(k.storeKey)
+	store.Delete(types.GetTokenizeShareRecordIdByOwnerAndIdKey(owner, id))
+}
+
+func (k Keeper) setTokenizeShareRecordWithDenom(ctx sdk.Context, denom string, id uint64) {
+	store := ctx.KVStore(k.storeKey)
+	bz := k.cdc.MustMarshal(&gogotypes.UInt64Value{Value: id})
+
+	store.Set(types.GetTokenizeShareRecordIdByDenomKey(denom), bz)
+}

persistenceOne/persistence-sdk

x/lsnative/distribution/client/cli/query.go
@@ -11,7 +11,8 @@ import (
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/version"
-	"github.com/cosmos/cosmos-sdk/x/distribution/types"
+	sdkdistr "github.com/cosmos/cosmos-sdk/x/distribution/types"
+	"github.com/persistenceOne/persistence-sdk/v2/x/lsnative/distribution/types"
 )
 
 // GetQueryCmd returns the cli query commands for this module
@@ -31,6 +32,7 @@ func GetQueryCmd() *cobra.Command {
 		GetCmdQueryValidatorSlashes(),
 		GetCmdQueryDelegatorRewards(),
 		GetCmdQueryCommunityPool(),
+		GetCmdQueryTokenizeShareRecordReward(),
 	)
 
 	return distQueryCmd
@@ -260,7 +262,7 @@ $ %s query distribution rewards %s1gghjut3ccd8ay0zduzj64hwre2fxs9ld75ru9p %s1ggh
 
 				res, err := queryClient.DelegationRewards(
 					ctx,
-					&types.QueryDelegationRewardsRequest{DelegatorAddress: delegatorAddr.String(), ValidatorAddress: validatorAddr.String()},
+					&sdkdistr.QueryDelegationRewardsRequest{DelegatorAddress: delegatorAddr.String(), ValidatorAddress: validatorAddr.String()},
 				)
 				if err != nil {
 					return err
@@ -308,6 +310,51 @@ $ %s query distribution community-pool
 			queryClient := types.NewQueryClient(clientCtx)
 
 			res, err := queryClient.CommunityPool(cmd.Context(), &types.QueryCommunityPoolRequest{})
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+	return cmd
+}
+
+// GetCmdQueryTokenizeShareRecordReward implements the query tokenize share record rewards
+func GetCmdQueryTokenizeShareRecordReward() *cobra.Command {
+	bech32PrefixAccAddr := sdk.GetConfig().GetBech32AccountAddrPrefix()
+
+	cmd := &cobra.Command{
+		Use:   "tokenize-share-record-rewards [owner]",
+		Args:  cobra.ExactArgs(1),
+		Short: "Query distribution tokenize share record rewards",
+		Long: strings.TrimSpace(
+			fmt.Sprintf(`Query the query tokenize share record rewards.
+
+Example:
+$ %s query distribution tokenize-share-record-rewards %s1gghjut3ccd8ay0zduzj64hwre2fxs9ldmqhffj
+`,
+				version.AppName, bech32PrefixAccAddr,
+			),
+		),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+			queryClient := types.NewQueryClient(clientCtx)
+
+			ownerAddr, err := sdk.AccAddressFromBech32(args[0])
+			if err != nil {
+				return err
+			}
+
+			res, err := queryClient.TokenizeShareRecordReward(
+				cmd.Context(),
+				&types.QueryTokenizeShareRecordRewardRequest{OwnerAddress: ownerAddr.String()},
+			)
 			if err != nil {
 				return err
 			}

x/lsnative/distribution/simulation/proposals.go
@@ -6,8 +6,8 @@ import (
 	simappparams "github.com/cosmos/cosmos-sdk/simapp/params"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	simtypes "github.com/cosmos/cosmos-sdk/types/simulation"
-	"github.com/cosmos/cosmos-sdk/x/distribution/keeper"
-	"github.com/cosmos/cosmos-sdk/x/distribution/types"
+	"github.com/persistenceOne/persistence-sdk/v2/x/lsnative/distribution/keeper"
+	"github.com/persistenceOne/persistence-sdk/v2/x/lsnative/distribution/types"
 	"github.com/cosmos/cosmos-sdk/x/simulation"
 )
 

x/lsnative/slashing/keeper/keeper.go
@@ -3,13 +3,13 @@ package keeper
 import (
 	"fmt"
 
-	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	"github.com/tendermint/tendermint/libs/log"
 
 	"github.com/cosmos/cosmos-sdk/codec"
 	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
+	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/cosmos/cosmos-sdk/x/slashing/types"
+	"github.com/persistenceOne/persistence-sdk/v2/x/lsnative/slashing/types"
 )
 
 // Keeper of the slashing store

x/lsnative/slashing/simulation/operations.go
@@ -11,9 +11,9 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	simtypes "github.com/cosmos/cosmos-sdk/types/simulation"
 	"github.com/cosmos/cosmos-sdk/x/simulation"
-	"github.com/cosmos/cosmos-sdk/x/slashing/keeper"
-	"github.com/cosmos/cosmos-sdk/x/slashing/types"
-	stakingkeeper "github.com/cosmos/cosmos-sdk/x/staking/keeper"
+	"github.com/persistenceOne/persistence-sdk/v2/x/lsnative/slashing/keeper"
+	"github.com/persistenceOne/persistence-sdk/v2/x/lsnative/slashing/types"
+	stakingkeeper "github.com/persistenceOne/persistence-sdk/v2/x/lsnative/staking/keeper"
 )
 
 // Simulation operation weights constants
@@ -109,17 +109,13 @@ func SimulateMsgUnjail(ak types.AccountKeeper, bk types.BankKeeper, k keeper.Kee
 		// - validator is still in jailed period
 		// - self delegation too low
 		if info.Tombstoned ||
-			ctx.BlockHeader().Time.Before(info.JailedUntil) ||
-			validator.TokensFromShares(selfDel.GetShares()).TruncateInt().LT(validator.GetMinSelfDelegation()) {
+			ctx.BlockHeader().Time.Before(info.JailedUntil) {
 			if res != nil && err == nil {
 				if info.Tombstoned {
 					return simtypes.NewOperationMsg(msg, true, "", nil), nil, errors.New("validator should not have been unjailed if validator tombstoned")
 				}
 				if ctx.BlockHeader().Time.Before(info.JailedUntil) {
 					return simtypes.NewOperationMsg(msg, true, "", nil), nil, errors.New("validator unjailed while validator still in jail period")
-				}
-				if validator.TokensFromShares(selfDel.GetShares()).TruncateInt().LT(validator.GetMinSelfDelegation()) {
-					return simtypes.NewOperationMsg(msg, true, "", nil), nil, errors.New("validator unjailed even though self-delegation too low")
 				}
 			}
 			// msg failed as expected

x/lsnative/staking/client/cli/flags.go
@@ -3,7 +3,7 @@ package cli
 import (
 	flag "github.com/spf13/pflag"
 
-	"github.com/cosmos/cosmos-sdk/x/staking/types"
+	"github.com/persistenceOne/persistence-sdk/v2/x/lsnative/staking/types"
 )
 
 const (
@@ -25,8 +25,6 @@ const (
 	FlagCommissionRate          = "commission-rate"
 	FlagCommissionMaxRate       = "commission-max-rate"
 	FlagCommissionMaxChangeRate = "commission-max-change-rate"
-
-	FlagMinSelfDelegation = "min-self-delegation"
 
 	FlagGenesisFormat = "genesis-format"
 	FlagNodeID        = "node-id"
@@ -56,13 +54,6 @@ func FlagSetCommissionCreate() *flag.FlagSet {
 	fs.String(FlagCommissionMaxRate, "", "The maximum commission rate percentage")
 	fs.String(FlagCommissionMaxChangeRate, "", "The maximum commission change rate percentage (per day)")
 
-	return fs
-}
-
-// FlagSetMinSelfDelegation Returns the FlagSet used for minimum set delegation.
-func FlagSetMinSelfDelegation() *flag.FlagSet {
-	fs := flag.NewFlagSet("", flag.ContinueOnError)
-	fs.String(FlagMinSelfDelegation, "", "The minimum self delegation required on the validator")
 	return fs
 }
 

x/lsnative/staking/keeper/pool.go
@@ -4,7 +4,7 @@ import (
 	"cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
-	"github.com/cosmos/cosmos-sdk/x/staking/types"
+	"github.com/persistenceOne/persistence-sdk/v2/x/lsnative/staking/types"
 )
 
 // GetBondedPool returns the bonded tokens pool's module account

x/lsnative/staking/keeper/tokenize_share_record.go
@@ -1,0 +1,150 @@
+package keeper
+
+import (
+	"fmt"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	gogotypes "github.com/gogo/protobuf/types"
+
+	"github.com/persistenceOne/persistence-sdk/v2/x/lsnative/staking/types"
+)
+
+func (k Keeper) GetLastTokenizeShareRecordId(ctx sdk.Context) uint64 {
+	store := ctx.KVStore(k.storeKey)
+	bytes := store.Get(types.LastTokenizeShareRecordIdKey)
+	if bytes == nil {
+		return 0
+	}
+	return sdk.BigEndianToUint64(bytes)
+}
+
+func (k Keeper) SetLastTokenizeShareRecordId(ctx sdk.Context, id uint64) {
+	store := ctx.KVStore(k.storeKey)
+	store.Set(types.LastTokenizeShareRecordIdKey, sdk.Uint64ToBigEndian(id))
+}
+
+func (k Keeper) GetTokenizeShareRecord(ctx sdk.Context, id uint64) (tokenizeShareRecord types.TokenizeShareRecord, err error) {
+	store := ctx.KVStore(k.storeKey)
+
+	bz := store.Get(types.GetTokenizeShareRecordByIndexKey(id))
+	if bz == nil {
+		return tokenizeShareRecord, sdkerrors.Wrap(types.ErrTokenizeShareRecordNotExists, fmt.Sprintf("tokenizeShareRecord %d does not exist", id))
+	}
+
+	k.cdc.MustUnmarshal(bz, &tokenizeShareRecord)
+	return tokenizeShareRecord, nil
+}
+
+func (k Keeper) GetTokenizeShareRecordsByOwner(ctx sdk.Context, owner sdk.AccAddress) (tokenizeShareRecords []types.TokenizeShareRecord) {
+	store := ctx.KVStore(k.storeKey)
+
+	var it sdk.Iterator = sdk.KVStorePrefixIterator(store, types.GetTokenizeShareRecordIdsByOwnerPrefix(owner))
+	defer it.Close()
+
+	for ; it.Valid(); it.Next() {
+		var id gogotypes.UInt64Value
+		k.cdc.MustUnmarshal(it.Value(), &id)
+
+		tokenizeShareRecord, err := k.GetTokenizeShareRecord(ctx, id.Value)
+		if err != nil {
+			continue
+		}
+		tokenizeShareRecords = append(tokenizeShareRecords, tokenizeShareRecord)
+	}
+	return
+}
+
+func (k Keeper) GetTokenizeShareRecordByDenom(ctx sdk.Context, denom string) (types.TokenizeShareRecord, error) {
+	store := ctx.KVStore(k.storeKey)
+	bz := store.Get(types.GetTokenizeShareRecordIdByDenomKey(denom))
+	if bz == nil {
+		return types.TokenizeShareRecord{}, fmt.Errorf("tokenize share record not found from denom: %s", denom)
+	}
+
+	var id gogotypes.UInt64Value
+	k.cdc.MustUnmarshal(bz, &id)
+
+	return k.GetTokenizeShareRecord(ctx, id.Value)
+}
+
+func (k Keeper) GetAllTokenizeShareRecords(ctx sdk.Context) (tokenizeShareRecords []types.TokenizeShareRecord) {
+	store := ctx.KVStore(k.storeKey)
+
+	var it sdk.Iterator = sdk.KVStorePrefixIterator(store, types.TokenizeShareRecordPrefix)
+	defer it.Close()
+
+	for ; it.Valid(); it.Next() {
+		var tokenizeShareRecord types.TokenizeShareRecord
+		k.cdc.MustUnmarshal(it.Value(), &tokenizeShareRecord)
+
+		tokenizeShareRecords = append(tokenizeShareRecords, tokenizeShareRecord)
+	}
+	return
+}
+
+func (k Keeper) AddTokenizeShareRecord(ctx sdk.Context, tokenizeShareRecord types.TokenizeShareRecord) error {
+	if k.hasTokenizeShareRecord(ctx, tokenizeShareRecord.Id) {
+		return sdkerrors.Wrapf(types.ErrTokenizeShareRecordAlreadyExists, "TokenizeShareRecord already exists: %d", tokenizeShareRecord.Id)
+	}
+
+	k.setTokenizeShareRecord(ctx, tokenizeShareRecord)
+
+	owner, err := sdk.AccAddressFromBech32(tokenizeShareRecord.Owner)
+	if err != nil {
+		return err
+	}
+
+	k.setTokenizeShareRecordWithOwner(ctx, owner, tokenizeShareRecord.Id)
+	k.setTokenizeShareRecordWithDenom(ctx, tokenizeShareRecord.GetShareTokenDenom(), tokenizeShareRecord.Id)
+
+	return nil
+}
+
+func (k Keeper) DeleteTokenizeShareRecord(ctx sdk.Context, recordId uint64) error {
+	record, err := k.GetTokenizeShareRecord(ctx, recordId)
+	if err != nil {
+		return err
+	}
+	owner, err := sdk.AccAddressFromBech32(record.Owner)
+	if err != nil {
+		return err
+	}
+
+	store := ctx.KVStore(k.storeKey)
+	store.Delete(types.GetTokenizeShareRecordByIndexKey(recordId))
+	store.Delete(types.GetTokenizeShareRecordIdByOwnerAndIdKey(owner, recordId))
+	store.Delete(types.GetTokenizeShareRecordIdByDenomKey(record.GetShareTokenDenom()))
+	return nil
+}
+
+func (k Keeper) hasTokenizeShareRecord(ctx sdk.Context, id uint64) bool {
+	store := ctx.KVStore(k.storeKey)
+	return store.Has(types.GetTokenizeShareRecordByIndexKey(id))
+}
+
+func (k Keeper) setTokenizeShareRecord(ctx sdk.Context, tokenizeShareRecord types.TokenizeShareRecord) {
+	store := ctx.KVStore(k.storeKey)
+	bz := k.cdc.MustMarshal(&tokenizeShareRecord)
+
+	store.Set(types.GetTokenizeShareRecordByIndexKey(tokenizeShareRecord.Id), bz)
+}
+
+func (k Keeper) setTokenizeShareRecordWithOwner(ctx sdk.Context, owner sdk.AccAddress, id uint64) {
+	store := ctx.KVStore(k.storeKey)
+	bz := k.cdc.MustMarshal(&gogotypes.UInt64Value{Value: id})
+
+	store.Set(types.GetTokenizeShareRecordIdByOwnerAndIdKey(owner, id), bz)
+}
+
+func (k Keeper) deleteTokenizeShareRecordWithOwner(ctx sdk.Context, owner sdk.AccAddress, id uint64) {
+	store := ctx.KVStore(k.storeKey)
+	store.Delete(types.GetTokenizeShareRecordIdByOwnerAndIdKey(owner, id))
+}
+
+func (k Keeper) setTokenizeShareRecordWithDenom(ctx sdk.Context, denom string, id uint64) {
+	store := ctx.KVStore(k.storeKey)
+	bz := k.cdc.MustMarshal(&gogotypes.UInt64Value{Value: id})
+
+	store.Set(types.GetTokenizeShareRecordIdByDenomKey(denom), bz)
+}