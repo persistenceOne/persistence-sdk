f363a48ca7d3271e669acdfb9e2b62dbf718573b
puneetmahajan <puneetmahajan1996@gmail.com>
2025-09-08 16:06:45 +0400

    fix mods and tests

--- simapp/simd/cmd/testnet.go
+++ simapp/simd/cmd/testnet.go
@@ -306,6 +306,7 @@ func initTestnetFiles(
 			sdk.NewCoin(sdk.DefaultBondDenom, valTokens),
 			stakingtypes.NewDescription(nodeDirName, "", "", "", ""),
 			stakingtypes.NewCommissionRates(math.LegacyOneDec(), math.LegacyOneDec(), math.LegacyOneDec()),
+			math.OneInt(),
 		)
 		if err != nil {
 			return err
